c810464f5761dda8fec05377ee679a8fc61877b0
AdamEli <adam.elitzur@gmail.com>
2020-06-15 13:36:59 -0400

    Expanded User Input Choices.
    Allowed the user input to be the crypto's name, like Bitcoin, instead of just its ticker, like BTC.

--- Crypto Prices.py
+++ Crypto Prices.py
@@ -1,11 +1,31 @@
 import requests
+from bs4 import BeautifulSoup
 
-crypto = input("What Cryptocurrency Ticker would you like to know the price of? ").upper()
-url = 'https://api.binance.com/api/v1/ticker/price?symbol=' + crypto + "USDT"
-response = requests.get(
-    url,
-    headers = {"Accept": "application/json"}
-).json()
+crypto = input("What cryptocurrency would you like to know the price of? ")
+def cost(ticker):
+    """Finds the price of the ticker"""
+    url = 'https://api.binance.com/api/v1/ticker/price?symbol=' + ticker.upper() + "USDT"
+    response = requests.get(
+        url,
+        headers = {"Accept": "application/json"}
+        ).json()
+    price = response["price"]
+    return f"The price of {ticker} is ${price}"
 
-price = response["price"]
-print(price)
+def convert(name):
+    """If the user input isn't a crypto's ticker, but its name, like Bitcoin instead of BTC, this function will convert it"""
+    tickers = requests.get('https://coinmarketcap.com/currencies/' + name.lower())
+    soup = BeautifulSoup(tickers.text, "html.parser")
+    el = soup.find(class_ = 'cmc-details-panel-header sc-1extin6-0 gMbCkP')
+    y = el.find_all('span')[1]
+    text = y.get_text()
+    ticker = text[1:-1]
+    return cost(ticker)
+
+try:
+    try:
+        print(cost(crypto))
+    except:
+        print(convert(crypto))
+except:
+    raise ValueError("That cryptocurrency is not supported on Binance")
