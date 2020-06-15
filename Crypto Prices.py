002eb614b7d76b02e3e982842782ea03668ed0e5
AdamEli <adam.elitzur@gmail.com>
2020-06-15 10:25:18 -0400

    Added .upper() to prevent an error.

--- Crypto Prices.py
+++ Crypto Prices.py
@@ -1,6 +1,6 @@
 import requests
 
-crypto = input("What Cryptocurrency Ticker would you like to know the price of? ")
+crypto = input("What Cryptocurrency Ticker would you like to know the price of? ").upper()
 url = 'https://api.binance.com/api/v1/ticker/price?symbol=' + crypto + "USDT"
 response = requests.get(
     url,
