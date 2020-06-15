2fdf8e9ce4a378327cb927a10d7f5bb7413d9f9f
AdamEli <adam.elitzur@gmail.com>
2020-06-14 22:16:13 -0400

    Add files via upload

---
+++ Crypto Prices.py
@@ -0,0 +1,11 @@
+import requests
+
+crypto = input("What Cryptocurrency Ticker would you like to know the price of? ")
+url = 'https://api.binance.com/api/v1/ticker/price?symbol=' + crypto + "USDT"
+response = requests.get(
+    url,
+    headers = {"Accept": "application/json"}
+).json()
+
+price = response["price"]
+print(price)
